91a6d1745f0e94771ff4f646991ddffa61757040
Anton Volkov <antonwolfy@gmail.com>
2023-09-06 00:01:47 +0200

    Run test_join with gdb

--- dpnp/__init__.py
+++ dpnp/__init__.py
@@ -29,7 +29,8 @@ import os
 mypath = os.path.dirname(os.path.realpath(__file__))
 
 # workaround against hanging in OneMKL calls and in DPCTL
-# os.environ.setdefault("SYCL_QUEUE_THREAD_POOL_SIZE", "60")
+# w/a to comment the line below
+os.environ.setdefault("SYCL_QUEUE_THREAD_POOL_SIZE", "6")
 
 import dpctl
 
